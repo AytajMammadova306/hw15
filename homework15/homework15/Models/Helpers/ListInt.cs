cc3d48714a46b021fa6f829f05a7f3eb7f0b25bb
AytajMammadova306 <aytajamam@code.edu.az>
2025-08-17 22:22:55 +0400

    initial

---
+++ homework15/homework15/Models/Helpers/ListInt.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework15.Models.Helpers
+{
+    internal class ListInt
+    {
+        private int[] arr = {0,1,2,3,4,5};
+        public int this[int i]
+        {
+            get { return arr[i]; }
+            set { arr[i] = value; }
+        }
+        public void Add(int num)
+        {
+            int[] newarr = new int[arr.Length + 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newarr[i] = arr[i];
+            }
+            newarr[newarr.Length -1] = num;
+            arr = newarr;
+        }
+        public bool Contains(int num)
+        {
+            foreach (int i in arr)
+            {
+                if (i == num)
+                    return true;
+            }
+            return false;
+        }
+        public void AddRange(params int[] nums)
+        {
+            int[] newarr = new int[nums.Length + arr.Length];
+            for(int i=0; i < arr.Length; i++)
+            {
+                newarr[i] = arr[i];
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                newarr[i + arr.Length] = nums[i];
+            }
+            arr = newarr;
+        }
+        public int Sum()
+        {
+            int sum = 0;
+            foreach(int i in arr)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public void Remove(int num)
+        {
+            int[] newarr = new int[arr.Length - 1];
+            int j = 0;
+            for (int i = 0;i < arr.Length;i++)
+            {
+                if (arr[i] != num)
+                {
+                    newarr[j]=arr[i];
+                    j++;
+                }
+            }
+            arr = newarr;
+        }
+        public void RemoveRange(params int[] nums)
+        {
+            int[] newarr = new int[arr.Length - nums.Length];
+            int j = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int count = 0;
+                foreach (int k in nums)
+                {
+                    if (arr[i] !=k)
+                        count++;
+                }
+                if (count==nums.Length)
+                {
+                    newarr[j] = arr[i];
+                    j++;
+                }
+            }
+            arr = newarr;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                sb.Append($"{arr[i].ToString()}, ");
+            }
+            sb.Append(arr[arr.Length - 1]);
+            return sb.ToString();
+        }
+    }
+}
---
+++ homework15/homework15/Program.cs
@@ -0,0 +1,24 @@
+﻿using homework15.Models.Helpers;
+
+namespace homework15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListInt list = new ListInt();
+            list.Add(6);
+            Console.WriteLine(list[6]);
+            Console.WriteLine(list.Contains(6));
+            Console.WriteLine(list.Contains(7));
+            list.AddRange(7, 8, 9,10);
+            //Console.WriteLine(list.Sum());
+            //list.Remove(10);
+            //Console.WriteLine(list);
+            list.RemoveRange(5,10);
+            Console.WriteLine(list.Sum());
+            //Console.WriteLine(list);
+            
+        }
+    }
+}
