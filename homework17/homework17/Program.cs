28a8000c4a69324b5ec0bdfc0ef6ef537afe13c0
AytajMammadova306 <aytajamam@code.edu.az>
2025-08-21 16:18:47 +0400

    optional with bugs

---
+++ homework17/homework17/Program.cs
@@ -0,0 +1,33 @@
+﻿using homework17.Models;
+
+namespace homework17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair<int, string> pair = new Pair<int, string>
+            {
+                Key=1,
+                Value="samebody"
+            };
+            Pair<int, int> pair2 = new Pair<int, int>
+            {
+                Key = 2,
+                Value = 102
+            };
+            Pair<int, int> pair3 = new Pair<int, int>
+            {
+                Key = 2,
+                Value = 102
+            };
+            Pair<int, string>.Add(pair);
+            Pair<int, int>.Add(pair2);
+            Pair<int, int>.Add(pair2);
+            foreach (Pair<int, string> p in Pair<int, string>[])
+            {
+                Console.WriteLine($"Key:{Key}, Value:{Value}");
+            }
+        }
+    }
+}
